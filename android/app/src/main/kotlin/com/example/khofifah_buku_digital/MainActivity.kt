d13950ba0d4fb21ae55fa81509e538001b6321c7
khofifah <khofifahmgm@gmail.com>
2020-06-17 13:45:09 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/khofifah_buku_digital/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.khofifah_buku_digital
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
