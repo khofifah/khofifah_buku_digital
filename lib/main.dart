a2c1e00596657947a267910c96754378df42df3a
khofifah <khofifahmgm@gmail.com>
2020-06-17 16:05:23 +0700

    add header and phote profile

--- lib/main.dart
+++ lib/main.dart
@@ -35,22 +35,109 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        leading: Icon(Icons.menu),
+        title: Text("Buku Digitalku 4.0"),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(
+                Icons.search,
+                size: 26.0,
+              ),
+            )
+          ),
+        ],
+        elevation: 0.0,
+        backgroundColor: Color(0xFF363563)
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          height: height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[              
+              Container(
+                height: height * 0.4,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      child: Container(
+                        width: width,
+                        height: height * 0.34,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: AssetImage("assets/top_home.png"), fit: BoxFit.fitWidth),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(15, 50, 0, 0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Hai!",                          
+                              style: TextStyle(fontSize: 20, color: Colors.white)
+                            ),
+                            Text(
+                              "Khofifah",                          
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white)
+                            ),
+                            SizedBox(height: 20),
+                            Container(
+                              width: 100.0,
+                              height: 100.0,
+                              decoration: new BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: new DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: new NetworkImage(
+                                          "https://st4.depositphotos.com/4173075/28583/i/1600/depositphotos_285833324-stock-photo-trendy-poster-koala-watercolor-cartoon.jpg")
+                                  )
+                              )
+                            ),
+                          ],
+                        )
+                      ),
+                    ),
+                    Positioned(
+                      top: 230,
+                      left: 120,
+                      child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                        decoration: new BoxDecoration(
+                          color: Color(0xFF363563),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 15),
+                            Text(
+                              "Kelas",                          
+                              style: TextStyle(fontSize: 15, color: Colors.white)
+                            ),
+                            Text(
+                              "XI",                          
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45, color: Colors.white)
+                            ),
+                          ],
+                        )
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
